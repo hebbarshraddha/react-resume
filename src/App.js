5506dc1f98195744f9124bad4a46249fcb7be87a
hebbarshraddha <shraddha.21ad050@sode-edu.in>
2023-11-03 10:24:53 +0530

    added resume

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Resume2 from './components/Resume2/Resume2';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */
+      <Resume2/>}
     </div>
   );
 }
---
+++ src/components/Resume2/Resume2.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import image from '../../photo.jpg';
+import './Resume2.css';
+
+function Resume2() {
+  return (
+    <form><div>
+    <img src={image} className="App-img" alt='image' />
+    <p>
+      Shraddha Hebbar
+    </p>
+    <h1>Information</h1>
+    <h2>Name : Shraddha Hebbar</h2>
+    <h2>Email : shraddha.21ad050@sode-edu.in</h2>
+    <h2>Phone :7457012382</h2>
+    <centre>
+    <h2 >LinkedIn:<a
+    >" https://www.linkedin.com/in/shraddha-hebbar-199581254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">&#8226;https://www.linkedin.com/in/shraddha-hebbar-199581254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app/"</a></h2></centre>
+    <h1>About</h1>
+    <h2>I am Shraddha Hebbar, currently pursuing my Bachelor's in Engineering at Shri Madhwa Vadiraja Institute of Technology and Management, Bantakal. I am ambitious,punctual, hardworking person and put my full effort in the work I do. 
+</h2>
+    <h1>Education</h1>
+    <h2>B.E (Pursuing) <h2 />
+      Shri Madhwa Vadhiraja Institute of Technology and
+      Managements,Bantakal Udupi.
+      CGPA:9.00
+    </h2>
+    <h2>Education: Sri Venkataramana P U College, Kundapura (2021)</h2>
+    <h2>Percentage: 100%</h2>
+    <h2>Sri Venkataramana English Medium High School,Kundapura (2019)</h2>
+    <h2>Percentage:97.12%</h2>
+    <h1>Technical skills </h1>
+    <h2>➢ Programing Language: C, C++, JAVA, PYTHON, HTML</h2>
+    <h2>➢ Technical _MS Word.</h2>
+    <h2>➢Html , CSS and Javascript </h2>
+    <h1>Achievements/Certification </h1>
+    <h2>➢IEEE Eu-reka competition </h2>
+    <h2>➢ Workshop on Introduction to Git-Hub </h2>
+    
+  </div></form>
+    
+
+
+  )
+
+
+}
+
+
+export default Resume2
